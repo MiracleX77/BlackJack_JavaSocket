4923ddb901bf8b0fdd0cbf9c2f0b78bfecbbcbf7
Phatchara_Mix <99187452+MiracleX77@users.noreply.github.com>
2023-05-21 22:52:24 +0700

    .

--- BlackJack_JavaSocket/Client/Client.java
+++ BlackJack_JavaSocket/Client/Client.java
@@ -8,37 +8,34 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Client {
-    private static final int PORT_NUMBER= 7600;
+    private static final int PORT_NUMBER = 7600;
 
     private static Socket socket;
     private static BufferedReader reader;
 
     private static PrintWriter writer;
-    private static  Scanner scanner;
 
-    private static  MainGui mainGui;
-
+    private static MainGui mainGui;
 
     private static ArrayList<ArrayList<Integer>> players = new ArrayList<>();
 
-
     public static void main(String[] args) throws IOException {
         Client client = new Client();
-        client.connect("localhost",PORT_NUMBER);
+        client.connect("localhost", PORT_NUMBER);
         client.genGui();
         String message;
-        while(true){
+        while (true) {
             players.clear();
-            message= recFromServer();
-//            System.out.println(message);
-            if(!Objects.equals(message,"N")) {
+            message = recFromServer();
+            // System.out.println(message);
+            if (!Objects.equals(message, "N")) {
                 while (true) {
-                    if(message.charAt(0) == '1'){
+                    if (message.charAt(0) == '1') {
                         convertStringToArrayList(message);
 
                         mainGui.updateCardDealer(players.get(0).get(1));
                         mainGui.updateCardDealer(-1);
-                        for(int i =1;i<players.get(1).size();i++){
+                        for (int i = 1; i < players.get(1).size(); i++) {
                             mainGui.updateCardPlayer(players.get(1).get(i));
                         }
                         mainGui.updateScorePlayer(players.get(1).get(0));
@@ -46,8 +43,7 @@ public class Client {
 
                         mainGui.updateGui();
 
-                    }
-                    else  if(message.charAt(0) == '2'){
+                    } else if (message.charAt(0) == '2') {
                         String messages = message.substring(1);
                         String[] str = messages.split(" ");
                         int hit_card = Integer.parseInt(str[0]);
@@ -59,20 +55,19 @@ public class Client {
 
                         mainGui.updateGui();
 
-                        players.get(1).set(0,new_score);
+                        players.get(1).set(0, new_score);
                         players.get(1).add(hit_card);
 
-                        if(!status_player){
+                        if (!status_player) {
                             mainGui.youLost();
                             mainGui.updateGui();
                             break;
                         }
-                    }
-                    else  if(message.charAt(0) == '3'){
+                    } else if (message.charAt(0) == '3') {
 
                         convertStringToArrayList(message);
 
-                        for(int i =2;i<players.get(0).size();i++){
+                        for (int i = 2; i < players.get(0).size(); i++) {
                             mainGui.updateCardDealer(players.get(0).get(i));
                         }
                         mainGui.updateScoreDealer(players.get(0).get(0));
@@ -80,15 +75,12 @@ public class Client {
 
                         sendToServer("result");
 
-                    }
-                    else  if(message.charAt(0) == '4'){
-                        if(message.substring(1).equals("win")){
+                    } else if (message.charAt(0) == '4') {
+                        if (message.substring(1).equals("win")) {
                             mainGui.youWin();
-                        }
-                        else if (message.substring(1).equals("draw")){
+                        } else if (message.substring(1).equals("draw")) {
                             mainGui.youDraw();
-                        }
-                        else{
+                        } else {
                             mainGui.youLost();
                         }
                         mainGui.updateGui();
@@ -97,8 +89,7 @@ public class Client {
 
                     message = recFromServer();
                 }
-            }
-            else {
+            } else {
                 break;
             }
         }
@@ -106,45 +97,46 @@ public class Client {
 
     }
 
-    public  void connect(String serIp,int serPort) throws IOException{
-        socket = new Socket(serIp,serPort);
+    public void connect(String serIp, int serPort) throws IOException {
+        socket = new Socket(serIp, serPort);
         System.out.println("Connected");
         reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         OutputStream output = socket.getOutputStream();
-        writer = new PrintWriter(output,true);
-        scanner = new Scanner(System.in);
+        writer = new PrintWriter(output, true);
     }
 
-    public void genGui(){
+    public void genGui() {
         MainGui mainFrame = new MainGui(this);
         mainFrame.setVisible(true);
         mainGui = mainFrame;
 
     }
 
-    public static void sendToServer(String message){
+    public static void sendToServer(String message) {
         writer.println(message);
     }
+
     public static String recFromServer() throws IOException {
         return reader.readLine();
     }
-    public static void convertStringToArrayList(String message){
+
+    public static void convertStringToArrayList(String message) {
         players.clear();
-        //1[[35, 48], [43, 40]]
-        String messages = message.substring(3,message.length()-2);
-        //  35, 48], [43, 40
+        // 1[[35, 48], [43, 40]]
+        String messages = message.substring(3, message.length() - 2);
+        // 35, 48], [43, 40
         String[] str = messages.split("], \\[");
-        //  35, 48 and 43, 40
-        for (String s : str){
+        // 35, 48 and 43, 40
+        for (String s : str) {
             ArrayList<Integer> subList = new ArrayList<>();
             // 35 and 48
             String[] cards = s.split(", ");
-            for(String card: cards){
-                //convert string to int and add to subList
+            for (String card : cards) {
+                // convert string to int and add to subList
                 subList.add(Integer.parseInt(card));
             }
             players.add(subList);
-            //[[35, 48]]
+            // [[35, 48]]
         }
     }
 
--- BlackJack_JavaSocket/Server/Server.java
+++ BlackJack_JavaSocket/Server/Server.java
@@ -3,36 +3,26 @@ package BlackJack_JavaSocket.Server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 
 public class Server {
     private static final int PORT_NUMBER = 7600;
-
-
 
     public static void main(String[] args) throws IOException {
         try {
             ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
             System.out.println("Server started.");
 
-            ExecutorService executorService = Executors.newCachedThreadPool();
-
-            while(true){
+            while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Connected");
 
-                executorService.execute(new ClientHandle(clientSocket));
+                Thread thread = new Thread(new ClientHandle(clientSocket));
+                thread.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
-
-
-
-
 
 }
